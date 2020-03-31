ea986fe78eb549c0932e9f8a26421fcf58e4d58c
Khondakar Shefat Hossain <61610790+sifathossain39@users.noreply.github.com>
2020-03-31 12:39:28 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage ="images/"+"dice"+randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomDiceImage);
+
+// for image 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage2 = "images/" + "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+
+// if player 1 win
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML =" Player 1 Win";
+}
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Win ";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
